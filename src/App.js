e303ca166f2eef5b75482680972d11b145426b45
Nathan Moritz <nateim3@gmail.com>
2020-04-30 12:19:58 -0400

    Added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from "react";
 import PhotoAlbum from "./pages/PhotoAlbum";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 export class App extends Component {
   render() {
     return (
       <Fragment>
         <NavBar />
+        <Home />
         <PhotoAlbum />
       </Fragment>
     );
